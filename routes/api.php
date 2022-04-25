574834762eab7297dcb63fb9bc218f5f336f47b6
AbderrahmenBmz <abdogray@gmail.com>
2022-04-26 00:34:06 +0200

    first commit

---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource(
+        '/categories',
+        \App\Http\Controllers\Api\CategoryController::class
+    );
+});
+
+
+// Route::post('/auth/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+// Route::post('/auth/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+// Route::post('/auth/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = \App\Models\User::where('email', $request->email)->first();
+
+    if (! $user || ! \Illuminate\Support\Facades\Hash::check($request->password, $user->password)) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
